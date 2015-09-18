9d62c2e78977e968be8cd349b8f8863bfa8db821
Gabriel Caudrelier <gabriel.caudrelier@gmail.com>
2015-09-18 16:21:43 -0500

    version bump

--- version.h
+++ version.h
@@ -13,10 +13,10 @@ Released under AGPL see LICENSE for more information
 
 #define APPNAME "pip3line"
 
-#define VERSION_STRING "2.6.4"
+#define VERSION_STRING "2.7"
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 6
-#define VERSION_REV 4
+#define VERSION_MINOR 7
+#define VERSION_REV 0
 
 #ifndef GIT_COMMIT
 #define GIT_COMMIT "Unkonwn"
